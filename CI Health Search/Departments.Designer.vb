09cfdf7af8e9590c4ceb0e1e03c4978275fc9277
ksimoes <ksimoes@cihealthgroup.com>
2025-07-02 14:17:14 -0500

    Staffing on Depts

--- CI Health Search/Departments.Designer.vb
+++ CI Health Search/Departments.Designer.vb
@@ -35,7 +35,6 @@ Partial Class Departments
         gbSalariesandOther = New GroupBox()
         Label6 = New Label()
         gbStaffing = New GroupBox()
-        Label7 = New Label()
         btnOutpatientDepartments = New Button()
         btnInpatientDepartments = New Button()
         btnQualityDepartments = New Button()
@@ -44,6 +43,31 @@ Partial Class Departments
         btnProfileDepartments = New Button()
         Button1 = New Button()
         Label8 = New Label()
+        Label7 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        Label18 = New Label()
+        Label19 = New Label()
+        Label20 = New Label()
+        Label21 = New Label()
+        Label22 = New Label()
+        Label23 = New Label()
+        Label24 = New Label()
+        Label25 = New Label()
+        Label26 = New Label()
+        Label27 = New Label()
+        Label28 = New Label()
+        Label29 = New Label()
+        Label30 = New Label()
+        Label31 = New Label()
+        Label32 = New Label()
         gbInpatientRoutineCostCenters.SuspendLayout()
         gbAncillaryServiceCost.SuspendLayout()
         gbOtherReimburse.SuspendLayout()
@@ -169,22 +193,37 @@ Partial Class Departments
         ' 
         ' gbStaffing
         ' 
+        gbStaffing.Controls.Add(Label32)
+        gbStaffing.Controls.Add(Label31)
+        gbStaffing.Controls.Add(Label30)
+        gbStaffing.Controls.Add(Label29)
+        gbStaffing.Controls.Add(Label28)
+        gbStaffing.Controls.Add(Label27)
+        gbStaffing.Controls.Add(Label26)
+        gbStaffing.Controls.Add(Label25)
+        gbStaffing.Controls.Add(Label24)
+        gbStaffing.Controls.Add(Label23)
+        gbStaffing.Controls.Add(Label22)
+        gbStaffing.Controls.Add(Label21)
+        gbStaffing.Controls.Add(Label20)
+        gbStaffing.Controls.Add(Label19)
+        gbStaffing.Controls.Add(Label18)
+        gbStaffing.Controls.Add(Label17)
+        gbStaffing.Controls.Add(Label16)
+        gbStaffing.Controls.Add(Label15)
+        gbStaffing.Controls.Add(Label14)
+        gbStaffing.Controls.Add(Label13)
+        gbStaffing.Controls.Add(Label12)
+        gbStaffing.Controls.Add(Label11)
+        gbStaffing.Controls.Add(Label10)
+        gbStaffing.Controls.Add(Label9)
         gbStaffing.Controls.Add(Label7)
-        gbStaffing.Location = New Point(1250, 51)
+        gbStaffing.Location = New Point(1235, 41)
         gbStaffing.Name = "gbStaffing"
-        gbStaffing.Size = New Size(273, 84)
+        gbStaffing.Size = New Size(308, 476)
         gbStaffing.TabIndex = 4
         gbStaffing.TabStop = False
         gbStaffing.Text = "Staffing"
-        ' 
-        ' Label7
-        ' 
-        Label7.AutoSize = True
-        Label7.Location = New Point(38, 35)
-        Label7.Name = "Label7"
-        Label7.Size = New Size(165, 15)
-        Label7.TabIndex = 1
-        Label7.Text = "Probobaly Needs to be a table"
         ' 
         ' btnOutpatientDepartments
         ' 
@@ -259,6 +298,244 @@ Partial Class Departments
         Label8.TabIndex = 22
         Label8.Text = "A decent amount of info for this form is on the #11 or cost report data set"
         ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label7.Location = New Point(6, 45)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(47, 15)
+        Label7.TabIndex = 23
+        Label7.Text = "CRNA's"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label9.Location = New Point(6, 94)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(61, 15)
+        Label9.TabIndex = 24
+        Label9.Text = "Dieticians"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label10.Location = New Point(6, 129)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(122, 15)
+        Label10.TabIndex = 25
+        Label10.Text = "Inhalation Therapists"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label11.Location = New Point(6, 166)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(37, 15)
+        Label11.TabIndex = 26
+        Label11.Text = "LPN's"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label12.Location = New Point(6, 202)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(139, 15)
+        Label12.TabIndex = 27
+        Label12.Text = "Occupational Therapists"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label13.Location = New Point(6, 237)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(73, 15)
+        Label13.TabIndex = 28
+        Label13.Text = "Pharmacists"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label14.Location = New Point(6, 268)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(110, 15)
+        Label14.TabIndex = 29
+        Label14.Text = "Physical Therapists"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label15.Location = New Point(6, 304)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(114, 15)
+        Label15.TabIndex = 30
+        Label15.Text = "Physician Assistants"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label16.Location = New Point(6, 342)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(32, 15)
+        Label16.TabIndex = 31
+        Label16.Text = "RN's"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label17.Location = New Point(6, 376)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(183, 15)
+        Label17.TabIndex = 32
+        Label17.Text = "Speech Patholigists, Audioligists"
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label18.Location = New Point(6, 411)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(90, 15)
+        Label18.TabIndex = 33
+        Label18.Text = "Social Workers"
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label19.Location = New Point(6, 447)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(98, 15)
+        Label19.TabIndex = 34
+        Label19.Text = "Other Personnel"
+        ' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label20.Location = New Point(263, 9)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(34, 15)
+        Label20.TabIndex = 35
+        Label20.Text = "FTE's"
+        ' 
+        ' Label21
+        ' 
+        Label21.AutoSize = True
+        Label21.Location = New Point(263, 45)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(14, 15)
+        Label21.TabIndex = 36
+        Label21.Text = "#"
+        ' 
+        ' Label22
+        ' 
+        Label22.AutoSize = True
+        Label22.Location = New Point(263, 94)
+        Label22.Name = "Label22"
+        Label22.Size = New Size(14, 15)
+        Label22.TabIndex = 37
+        Label22.Text = "#"
+        ' 
+        ' Label23
+        ' 
+        Label23.AutoSize = True
+        Label23.Location = New Point(263, 129)
+        Label23.Name = "Label23"
+        Label23.Size = New Size(14, 15)
+        Label23.TabIndex = 38
+        Label23.Text = "#"
+        ' 
+        ' Label24
+        ' 
+        Label24.AutoSize = True
+        Label24.Location = New Point(263, 166)
+        Label24.Name = "Label24"
+        Label24.Size = New Size(14, 15)
+        Label24.TabIndex = 39
+        Label24.Text = "#"
+        ' 
+        ' Label25
+        ' 
+        Label25.AutoSize = True
+        Label25.Location = New Point(263, 202)
+        Label25.Name = "Label25"
+        Label25.Size = New Size(14, 15)
+        Label25.TabIndex = 40
+        Label25.Text = "#"
+        ' 
+        ' Label26
+        ' 
+        Label26.AutoSize = True
+        Label26.Location = New Point(263, 237)
+        Label26.Name = "Label26"
+        Label26.Size = New Size(14, 15)
+        Label26.TabIndex = 41
+        Label26.Text = "#"
+        ' 
+        ' Label27
+        ' 
+        Label27.AutoSize = True
+        Label27.Location = New Point(263, 268)
+        Label27.Name = "Label27"
+        Label27.Size = New Size(14, 15)
+        Label27.TabIndex = 42
+        Label27.Text = "#"
+        ' 
+        ' Label28
+        ' 
+        Label28.AutoSize = True
+        Label28.Location = New Point(263, 304)
+        Label28.Name = "Label28"
+        Label28.Size = New Size(14, 15)
+        Label28.TabIndex = 43
+        Label28.Text = "#"
+        ' 
+        ' Label29
+        ' 
+        Label29.AutoSize = True
+        Label29.Location = New Point(263, 342)
+        Label29.Name = "Label29"
+        Label29.Size = New Size(14, 15)
+        Label29.TabIndex = 44
+        Label29.Text = "#"
+        ' 
+        ' Label30
+        ' 
+        Label30.AutoSize = True
+        Label30.Location = New Point(263, 376)
+        Label30.Name = "Label30"
+        Label30.Size = New Size(14, 15)
+        Label30.TabIndex = 45
+        Label30.Text = "#"
+        ' 
+        ' Label31
+        ' 
+        Label31.AutoSize = True
+        Label31.Location = New Point(263, 411)
+        Label31.Name = "Label31"
+        Label31.Size = New Size(14, 15)
+        Label31.TabIndex = 46
+        Label31.Text = "#"
+        ' 
+        ' Label32
+        ' 
+        Label32.AutoSize = True
+        Label32.Location = New Point(263, 447)
+        Label32.Name = "Label32"
+        Label32.Size = New Size(14, 15)
+        Label32.TabIndex = 47
+        Label32.Text = "#"
+        ' 
         ' Departments
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
@@ -313,7 +590,6 @@ Partial Class Departments
     Friend WithEvents gbSalariesandOther As GroupBox
     Friend WithEvents Label6 As Label
     Friend WithEvents gbStaffing As GroupBox
-    Friend WithEvents Label7 As Label
     Friend WithEvents btnOutpatientDepartments As Button
     Friend WithEvents btnInpatientDepartments As Button
     Friend WithEvents btnQualityDepartments As Button
@@ -322,4 +598,29 @@ Partial Class Departments
     Friend WithEvents btnProfileDepartments As Button
     Friend WithEvents Button1 As Button
     Friend WithEvents Label8 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label32 As Label
+    Friend WithEvents Label31 As Label
+    Friend WithEvents Label30 As Label
+    Friend WithEvents Label29 As Label
+    Friend WithEvents Label28 As Label
+    Friend WithEvents Label27 As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
 End Class
